f0a7be5f63082e5a6ff400dd45b955f9356f6b03
Frikk Herding <frikksol@gmail.com>
2022-10-10 08:09:06 +0200

    💄 Make it completely monochrome

--- client/routes/index.svelte
+++ client/routes/index.svelte
@@ -11,9 +11,7 @@
       class="grid xl:grid-cols-2 grid-cols-1 xl:grid-flow-col grid-flow-row xl:px-60 px-6 xl:pt-60 pt-12"
     >
       <div>
-        <h1 class="sm:text-8xl text-5xl text-purple-700 font-bold">
-          Playground
-        </h1>
+        <h1 class="sm:text-8xl text-5xl text-white font-bold">Playground</h1>
       </div>
       <div class="xl:pt-28 pt-12">
         <p class="font-sans font-medium text-xl text-white">
