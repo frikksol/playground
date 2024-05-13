21799e5f9cb48d92b044977c2c83a6f164d4deb6
Frikk Herding <frikksol@gmail.com>
2024-05-13 20:40:22 +0200

    Tweak button underlines

--- src/components/uuid/Button.svelte
+++ src/components/uuid/Button.svelte
@@ -10,7 +10,7 @@
   on:mouseleave
   on:focus
   {...buttonProps}
-  class="mr-12 w-fit text-white hover:underline text-left text-3xl font-bold font-sansd"
+  class="mr-12 w-fit text-white underline hover:decoration-gray-400 text-left text-3xl font-bold font-sansd"
 >
   <h1>{text}</h1>
 </button>
--- src/components/uuid/SelectorItem.svelte
+++ src/components/uuid/SelectorItem.svelte
@@ -12,7 +12,7 @@
     on:mouseleave
     on:focus
     {...buttonProps}
-    class="mr-12 w-fit text-white hover:underline text-left text-3xl font-bold font-sans underline"
+    class="mr-12 w-fit text-white hover:decoration-gray-400 hover:underline text-left text-3xl font-bold font-sans underline"
   >
     <h1>{text}</h1>
   </button>
@@ -24,7 +24,7 @@
     on:mouseleave
     on:focus
     {...buttonProps}
-    class="mr-12 w-fit text-white hover:underline text-left text-3xl font-bold font-sansd"
+    class="mr-12 w-fit text-white hover:underline hover:decoration-gray-400 text-left text-3xl font-bold font-sansd"
   >
     <h1>{text}</h1>
   </button>
